72b6c2c3b9b18af264e70b5b97658a4bd43d1651
stevechan <chanshunli@gmail.com>
2025-06-04 20:58:49 +0800

    还是人才能打破局： python jim0jim1/curl_parse.py =》 https://www.xmwav.com/download/rmk13318724726.html

---
+++ jim0jim1/curl_parse.py
@@ -0,0 +1,16 @@
+import subprocess
+import re
+
+# 要执行的命令
+cmd = 'curl https://www.xmwav.com/mscdetail/133187.html | grep download | grep mp3音乐'
+
+# 执行命令并获取输出
+result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+
+# 提取 href 中的链接
+matches = re.findall(r'href="(https://[^"]+)"', result.stdout)
+
+# 打印提取到的链接
+for url in matches:
+    print(url)
+
